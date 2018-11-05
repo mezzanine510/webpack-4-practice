ea3b973fa37f13e3eb4407449a00a21d36f49105
Mezzanine <mezzanine510@gmail.com>
2018-11-05 14:39:58 -0800

    Added: extract-text-webpack-plugin to separate css into its own file

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     module: {
         rules: [
             {
                 test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                })
                 // applied last to first: css-loader first, then style-loader
-                use: [
-                    { loader: "style-loader" }, 
-                    { loader: "css-loader" }
-                ]
+                //     [
+                //     { loader: "style-loader" }, 
+                //     { loader: "css-loader" }
+                // ]
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
 };
